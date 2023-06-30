3e0807859630417f10f1c602c7bc87748af2c1b4
Saxar88 <v.sasaran@protonmail.com>
2023-06-30 18:06:52 +0300

    fix footer responsiveness issues

--- components/Footer.js
+++ components/Footer.js
@@ -1,66 +1,70 @@
-import React from "react";
+import React from 'react';
 
 function Footer() {
 	return (
-		<div className="flex justify-center mt-8 p-5 gap-x-28 bg-amazon_blue-light text-slate-300 text-sm leading-7">
-			<div>
-				<h5 className="text-white text-base font-medium">Get to Know Us</h5>
-				<ul>
-					<li className="link">Careers</li>
-					<li className="link">Press Releases</li>
-					<li className="link">About us</li>
-					<li className="link">Blog</li>
-					<li className="link">Imprint</li>
-					<li className="link">Amazon Science</li>
-				</ul>
+		<footer>
+			<div className='flex justify-center mt-8 p-5 gap-x-28 bg-amazon_blue-light text-slate-300 text-sm leading-7'>
+				<div>
+					<h5 className='text-white text-base font-medium'>Get to Know Us</h5>
+					<ul>
+						<li className='link'>Careers</li>
+						<li className='link'>Press Releases</li>
+						<li className='link'>About us</li>
+						<li className='link'>Blog</li>
+						<li className='link'>Imprint</li>
+						<li className='link'>Amazon Science</li>
+					</ul>
+				</div>
+				<div className='w-52'>
+					<h5 className='text-white text-base font-medium'>
+						Make Money with Us
+					</h5>
+					<ul>
+						<li className='link'>Sell on Amazon</li>
+						<li className='link'>Sell on Amazon Business</li>
+						<li className='link'>Sell on Amazon Handmade</li>
+						<li className='link'>Associates Programme</li>
+						<li className='link'>Fulfilment by Amazon</li>
+						<li className='link'>Seller Fulfilled Prime</li>
+						<li className='link'>Advertise Your Products</li>
+						<li className='link'>Independently Publish with Us</li>
+						<li className='link'>Amazon Pay</li>
+					</ul>
+				</div>
+				<div className='hidden md:flex flex-col w-52'>
+					<h5 className='text-white text-base font-medium'>
+						Amazon Payment Methods
+					</h5>
+					<ul>
+						<li className='link'>Amazon Visa Card</li>
+						<li className='link'>Shop with points</li>
+						<li className='link'>Credit Card</li>
+						<li className='link'>Gift Cards</li>
+						<li className='link'>Payment by Invoice</li>
+						<li className='link'>Direct Debit</li>
+						<li className='link'>Amazon Currency Converter</li>
+						<li className='link'>Top Up Your Account</li>
+					</ul>
+				</div>
+				<div className='hidden lg:flex flex-col w-52'>
+					<h5 className='text-white text-base font-medium'>Let Us Help You</h5>
+					<ul>
+						<li className='link'>Track Packages or View Orders</li>
+						<li className='link'>Delivery Rates & Policies</li>
+						<li className='link'>Returns & Replacements</li>
+						<li className='link leading-4'>
+							Recycling (including disposal of electrical and electronic
+							equipment)
+						</li>
+						<li className='link'>Manage Your Content and Devices</li>
+						<li className='link'>Amazon Mobile App</li>
+						<li className='link'>Amazon Assistant</li>
+						<li className='link'>Customer Service</li>
+						<li className='link'>Accessibility</li>
+					</ul>
+				</div>
 			</div>
-			<div className="w-52">
-				<h5 className="text-white text-base font-medium">Make Money with Us</h5>
-				<ul>
-					<li className="link">Sell on Amazon</li>
-					<li className="link">Sell on Amazon Business</li>
-					<li className="link">Sell on Amazon Handmade</li>
-					<li className="link">Associates Programme</li>
-					<li className="link">Fulfilment by Amazon</li>
-					<li className="link">Seller Fulfilled Prime</li>
-					<li className="link">Advertise Your Products</li>
-					<li className="link">Independently Publish with Us</li>
-					<li className="link">Amazon Pay</li>
-				</ul>
-			</div>
-			<div className="w-52">
-				<h5 className="text-white text-base font-medium">
-					Amazon Payment Methods
-				</h5>
-				<ul>
-					<li className="link">Amazon Visa Card</li>
-					<li className="link">Shop with points</li>
-					<li className="link">Credit Card</li>
-					<li className="link">Gift Cards</li>
-					<li className="link">Payment by Invoice</li>
-					<li className="link">Direct Debit</li>
-					<li className="link">Amazon Currency Converter</li>
-					<li className="link">Top Up Your Account</li>
-				</ul>
-			</div>
-			<div className="w-52">
-				<h5 className="text-white text-base font-medium">Let Us Help You</h5>
-				<ul>
-					<li className="link">Track Packages or View Orders</li>
-					<li className="link">Delivery Rates & Policies</li>
-					<li className="link">Returns & Replacements</li>
-					<li className="link leading-4">
-						Recycling (including disposal of electrical and electronic
-						equipment)
-					</li>
-					<li className="link">Manage Your Content and Devices</li>
-					<li className="link">Amazon Mobile App</li>
-					<li className="link">Amazon Assistant</li>
-					<li className="link">Customer Service</li>
-					<li className="link">Accessibility</li>
-				</ul>
-			</div>
-		</div>
+		</footer>
 	);
 }
 
--- components/Header.js
+++ components/Header.js
@@ -1,94 +1,94 @@
-import React from "react";
-import Image from "next/image";
-import {signIn, signOut, useSession} from "next-auth/react";
-import {useRouter} from "next/router";
-import {useSelector} from "react-redux";
+import React from 'react';
+import Image from 'next/image';
+import { signIn, signOut, useSession } from 'next-auth/react';
+import { useRouter } from 'next/router';
+import { useSelector } from 'react-redux';
 import {
 	Bars3Icon,
 	MagnifyingGlassIcon,
 	MapPinIcon,
 	ShoppingCartIcon,
-} from "@heroicons/react/24/outline";
-import {selectItems} from "../slices/basketSlice";
+} from '@heroicons/react/24/outline';
+import { selectItems } from '../slices/basketSlice';
 
 function Header() {
-	const {data: session} = useSession();
+	const { data: session } = useSession();
 	const router = useRouter();
 	const items = useSelector(selectItems);
 
 	return (
 		<header>
-			<div className="flex items-center bg-amazon_blue p-1 flex-grow py-2">
-				<div className="mt-2 flex items-center flex-grow sm:flex-grow-0">
+			<div className='flex items-center bg-amazon_blue p-1 flex-grow py-2'>
+				<div className='mt-2 flex items-center flex-grow sm:flex-grow-0'>
 					<Image
-						onClick={() => router.push("/")}
-						src={require("../public/logo.webp")}
-						alt="logo"
+						onClick={() => router.push('/')}
+						src={require('../public/logo.webp')}
+						alt='logo'
 						width={135}
 						height={50}
-						className="object-contain px-3 cursor-pointer"
+						className='object-contain px-3 cursor-pointer'
 					/>
 				</div>
-				<div className="hidden sm:flex text-white items-center text-xs ml-0 mr-6 whitespace-nowrap">
-					<MapPinIcon className="h-5 w-5 -mb-2" />
-					<div className="link">
+				<div className='hidden sm:flex text-white items-center text-xs ml-0 mr-6 whitespace-nowrap'>
+					<MapPinIcon className='h-5 w-5 -mb-2' />
+					<div className='link'>
 						<p>Deliver to</p>
-						<p className="font-extrabold md:text-sm">Romania</p>
+						<p className='font-extrabold md:text-sm'>Romania</p>
 					</div>
 				</div>
-				<div className="hidden sm:flex items-center h-10 rounded-md flex-grow cursor-pointer bg-yellow-400 hover:bg-yellow-500">
+				<div className='hidden sm:flex items-center h-10 rounded-md flex-grow cursor-pointer bg-yellow-400 hover:bg-yellow-500'>
 					<input
-						type="text"
-						className="p-2 h-full w-6 flex-grow flex-shrink rounded-l-md focus:outline-none px-4"
+						type='text'
+						className='p-2 h-full w-6 flex-grow flex-shrink rounded-l-md focus:outline-none px-4'
 					/>
-					<MagnifyingGlassIcon className="h-12 p-4" />
+					<MagnifyingGlassIcon className='h-12 p-4' />
 				</div>
-				<div className="text-white flex items-center text-xs space-x-6 mx-6 whitespace-nowrap">
-					<div className="link" onClick={!session ? signIn : signOut}>
+				<div className='text-white flex items-center text-xs space-x-6 mx-6 whitespace-nowrap'>
+					<div className='link' onClick={!session ? signIn : signOut}>
 						<p>{session ? `Hello,` : `Hello, sign in`}</p>
-						<p className="font-extrabold md:text-sm">Account & Lists</p>
+						<p className='font-extrabold md:text-sm'>Account & Lists</p>
 					</div>
 					<div
-						onClick={session ? () => router.push("/order-history") : signIn}
-						className="link">
+						onClick={session ? () => router.push('/order-history') : signIn}
+						className='link'>
 						<p>Returns</p>
-						<p className="font-extrabold md:text-sm">& Orders</p>
+						<p className='font-extrabold md:text-sm'>& Orders</p>
 					</div>
 
 					<div
-						onClick={() => router.push("/cart")}
-						className="relative link flex items-center">
-						<span className="absolute bottom-3 right-12 md:right-10 h-10 w-10 text-yellow-500 text-base font-bold">
+						onClick={() => router.push('/cart')}
+						className='relative link flex items-center'>
+						<span className='absolute bottom-3 right-12 md:right-10 h-10 w-10 text-yellow-500 text-base font-bold'>
 							{items.length}
 						</span>
-						<ShoppingCartIcon className="h-10" />
-						<div className="hidden md:inline">
+						<ShoppingCartIcon className='h-10' />
+						<div className='hidden md:inline'>
 							<p>Shopping-</p>
-							<p className="font-extrabold md:text-sm">Basket</p>
+							<p className='font-extrabold md:text-sm'>Basket</p>
 						</div>
 					</div>
 				</div>
 			</div>
-			<div className="flex items-center justify-between space-x-3 py-2 pl-6 pr-20 bg-amazon_blue-light text-white text-sm font-medium whitespace-nowrap">
-				<p className="link flex items-center font-bold">
-					<Bars3Icon className="h-6 mr-1" />
+			<div className='flex items-center justify-between space-x-3 py-2 pl-6 pr-20 bg-amazon_blue-light text-white text-sm font-medium whitespace-nowrap'>
+				<p className='link flex items-center font-bold'>
+					<Bars3Icon className='h-6 mr-1' />
 					All
 				</p>
-				<p className="link">Best Sellers</p>
-				<p className="link">Amazon Basics</p>
-				<p className="link">Today's Deals</p>
-				<p className="link">New Releases</p>
-				<p className="link hidden md:inline-flex">Costumer Service</p>
-				<p className="link hidden md:inline-flex">Music</p>
-				<p className="link hidden md:inline-flex">Kindle Books</p>
-				<p className="link hidden md:inline-flex">Books</p>
-				<p className="link hidden md:inline-flex">PC & Video Games</p>
-				<p className="link hidden md:inline-flex">Home & Kitchen</p>
-				<p className="link hidden md:inline-flex">Electronics & Photo</p>
-				<p className="link hidden lg:inline-flex">Fashion</p>
-				<p className="link hidden lg:inline-flex">Car & Motorbike</p>
-				<p className="link hidden lg:inline-flex">Toys & Games</p>
-				<p className="link hidden lg:inline-flex">Home Improvement</p>
+				<p className='link'>Best Sellers</p>
+				<p className='link'>Amazon Basics</p>
+				<p className='link'>Today's Deals</p>
+				<p className='link hidden md:inline-flex'>New Releases</p>
+				<p className='link hidden md:inline-flex'>Costumer Service</p>
+				<p className='link hidden md:inline-flex'>Music</p>
+				<p className='link hidden md:inline-flex'>Kindle Books</p>
+				<p className='link hidden md:inline-flex'>Books</p>
+				<p className='link hidden md:inline-flex'>PC & Video Games</p>
+				<p className='link hidden md:inline-flex'>Home & Kitchen</p>
+				<p className='link hidden md:inline-flex'>Electronics & Photo</p>
+				<p className='link hidden lg:inline-flex'>Fashion</p>
+				<p className='link hidden lg:inline-flex'>Car & Motorbike</p>
+				<p className='link hidden lg:inline-flex'>Toys & Games</p>
+				<p className='link hidden lg:inline-flex'>Home Improvement</p>
 			</div>
 		</header>
 	);
